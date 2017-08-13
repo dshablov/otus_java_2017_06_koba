8f42771e9a9a80ef40c196d17c05f6e4fa5fcbfa
vkoba <ricoshet@list.ru>
2017-08-13 23:04:27 +0300

    fix App

--- hw9/src/main/java/ru/otus/App.java
+++ hw9/src/main/java/ru/otus/App.java
@@ -3,8 +3,6 @@ package ru.otus;
 
 public class App {
     public static void main(String[] args) throws ClassNotFoundException {
-        // load the sqlite-JDBC driver using the current class loader
-        Class.forName("org.sqlite.JDBC");
 
     }
 
